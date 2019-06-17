1a557bbb57af21d15458512ee8fbed246272f511
Bartlomiej Zalas <bartlomiej.zalas@nokia.com>
2019-06-17 10:43:39 +0300

    expects added to the xlsReader test

--- data-import/tests/xlsReader.test.ts
+++ data-import/tests/xlsReader.test.ts
@@ -1,8 +1,18 @@
-import { readAntenasData } from '../src/xlsReader';
+import {readAntenasData} from '../src/xlsReader';
+import {expect} from 'chai';
 
 describe('XLSX Reader', () => {
     it('should convert data from xlsx to array of antenas', () => {
         const antenas = readAntenasData(__dirname + '/data/sample_data_test.xlsx');
-        console.log(antenas)
+
+        expect(antenas).to.have.lengthOf(14);
+
+        expect(antenas[0].lat).to.be.eq(49.86055555555556);
+        expect(antenas[0].lng).to.be.eq(19.038611111111113);
+        expect(antenas[0].name).to.be.eq('ul. Chochołowska 28');
+
+        expect(antenas[13].lat).to.be.eq(54.25963888888889);
+        expect(antenas[13].lng).to.be.eq(18.606);
+        expect(antenas[13].name).to.be.eq('ul. Różana 3');
     });
-})
+});
