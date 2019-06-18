18dd2dec9ad7c2c9224d6082dbb0cecf1962826e
Bartlomiej Zalas <bartlomiej.zalas@nokia.com>
2019-06-18 15:39:52 +0300

    geoJson format added

--- data-import/src/mongoDao.ts
+++ data-import/src/mongoDao.ts
@@ -4,13 +4,36 @@ import { MongoClient, MongoClientOptions } from 'mongodb';
 
 const url = "mongodb://localhost:27017/";
 
+interface MongoLocationFormat {
+    type: string,
+    coordinates: number[]
+}
+
+interface MongoBtsFromat {
+    name: string
+    location: MongoLocationFormat,
+}
+
+const convertBtsToDocument = (bts:Bts): MongoBtsFromat => {
+    return {
+        name: bts.name,
+        location: {
+            type: "Point",
+            coordinates: [bts.lng, bts.lat]
+        }
+    };
+};
+
 export const insertBtses: InsertBts = async(btses: Bts[]) => {
 
     const options: MongoClientOptions = {};
     const mongoClient = await MongoClient.connect(url, options);
     const database = mongoClient.db("btsMap");
     const btsCollection = database.collection('bts');
-    const result = await btsCollection.insertMany(btses);
+
+    const documents: MongoBtsFromat[] = btses.map(b => convertBtsToDocument(b));
+    const result = await btsCollection.insertMany(documents);
+
     await mongoClient.close();
 
     console.log(result);
